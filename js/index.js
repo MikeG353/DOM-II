d146d5216d36eeba4babc0d1a82e8b16d18258f5
Michael Gregory <michael.gregory8584@gmail.com>
2020-04-08 16:26:40 -0500

    first commit not done with MVP

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+let btnList = document.querySelectorAll('.destination .btn')
+btnList[0].addEventListener('click', (event) => { event.target.style.backgroundColor = 'red'; });
+btnList[1].addEventListener('click', (event) => { event.target.style.backgroundColor = 'white'; });
+btnList[2].addEventListener('click', (event) => { event.target.style.backgroundColor = 'blue'; });
+
+let imgChange = document.querySelector('.content-destination img')
+imgChange.addEventListener('mouseover', (event) => { event.target.src = 'img/island.jpg'});
