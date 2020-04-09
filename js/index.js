9cbf180df18c2afb9a16abca8aa4d014e574e82b
Michael Gregory <michael.gregory8584@gmail.com>
2020-04-08 21:05:16 -0500

    Prevent Default works 10 events added need to add propagation testing

--- js/index.js
+++ js/index.js
@@ -1,8 +1,30 @@
 // Your code goes here
-let btnList = document.querySelectorAll('.destination .btn')
+let btnList = document.querySelectorAll('.destination .btn');
 btnList[0].addEventListener('click', (event) => { event.target.style.backgroundColor = 'red'; });
 btnList[1].addEventListener('click', (event) => { event.target.style.backgroundColor = 'white'; });
 btnList[2].addEventListener('click', (event) => { event.target.style.backgroundColor = 'blue'; });
 
 let imgChange = document.querySelector('.content-destination img')
 imgChange.addEventListener('mouseover', (event) => { event.target.src = 'img/island.jpg'});
+
+let navList = document.querySelector('.nav');
+
+document.addEventListener('keydown', (blue) => { navList.style.backgroundColor = 'blue' });
+
+let trigger = document.querySelector('.logo-heading');
+trigger.addEventListener('dblclick', (green)=> {navList.style.backgroundColor = 'green' });
+
+trigger.addEventListener('drag', (red)=>{document.querySelector('header').style.backgroundColor = 'red'})
+
+trigger.addEventListener('dragend', (white)=>{document.querySelector('header').style.backgroundColor = 'white'})
+
+window.addEventListener('load', (pink)=> {navList.style.backgroundColor = 'pink'});
+
+window.addEventListener('beforeprint', (orange) => { navList.style.backgroundColor = 'orange' });
+
+window.addEventListener('afterprint', (yellow) => { navList.style.backgroundColor = 'yellow' });
+
+window.addEventListener('scroll', (purple) => { navList.style.backgroundColor = 'purple' });
+
+let links = document.querySelector('.nav-link');
+links.addEventListener('click', (event) => {event.preventDefault()});
