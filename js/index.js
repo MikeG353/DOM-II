976e2b0ff1a2c940de5d18bdfdc3838bda8c0f84
Michael Gregory <michael.gregory8584@gmail.com>
2020-04-08 21:35:51 -0500

    MVP

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
 // Your code goes here
 let btnList = document.querySelectorAll('.destination .btn');
-btnList[0].addEventListener('click', (event) => { event.target.style.backgroundColor = 'red'; });
+btnList[0].addEventListener('click', (event) => { event.target.style.backgroundColor = 'red'; 
+event.stopPropagation()});
 btnList[1].addEventListener('click', (event) => { event.target.style.backgroundColor = 'white'; });
 btnList[2].addEventListener('click', (event) => { event.target.style.backgroundColor = 'blue'; });
+let boxes = document.querySelector('.destination')
+boxes.addEventListener('click', (event) => { navList.style.backgroundColor = 'gray'})
 
 let imgChange = document.querySelector('.content-destination img')
 imgChange.addEventListener('mouseover', (event) => { event.target.src = 'img/island.jpg'});
